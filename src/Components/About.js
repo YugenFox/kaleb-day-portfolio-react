2fa02afa123a05ee8b6f47142eba48bdee84bcce
YugenFox <kalebsday@gmail.com>
2023-04-04 22:48:57 -0500

    Initial commit for my React based Portfolio

---
+++ src/Components/About.js
@@ -0,0 +1,8 @@
+const About = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laborum aliquid exercitationem inventore, tempore voluptate architecto. Doloribus nam in soluta at reprehenderit tempore magnam, sequi error dolorum non rerum quidem.</p>
+    </div>
+  )
+}
+export default About
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img></img>
+      <h1>Kaleb Day</h1>
+      <span>Software Engineer</span>
+    </header>
+  )
+}
+export default Header
