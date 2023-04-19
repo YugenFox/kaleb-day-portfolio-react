fcc9049ac5fe38bf48a3a720bfb146c2c540bb63
YugenFox <kalebsday@gmail.com>
2023-04-18 23:17:29 -0500

    Added reference ideas.txt and centered About txt

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,28 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 const About = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laborum aliquid exercitationem inventore, tempore voluptate architecto. Doloribus nam in soluta at reprehenderit tempore magnam, sequi error dolorum non rerum quidem.</p>
-    </div>
-  )
-}
-export default About
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography textAlign="center" variant="body1" color="Textprimary">
+        Please take a moment to look at my past work
+        <br />
+        Together there is something we can create/build to add value to your
+        current business or personal project :)
+      </Typography>
+    </Box>
+  );
+};
+export default About;
 
-{/* <div>
+{
+  /* <div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laborum aliquid exercitationem inventore, tempore voluptate architecto. Doloribus nam in soluta at reprehenderit tempore magnam, sequi error dolorum non rerum quidem.</p>
-    </div> */}
+    </div> */
+}
