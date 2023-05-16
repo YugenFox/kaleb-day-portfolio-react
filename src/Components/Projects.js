455bf39fedb3f36053b0f65c5537d72872d0b8cf
YugenFox <kalebsday@gmail.com>
2023-05-15 20:52:09 -0500

    Wrapped Box in Projects with Container, so would be centered on all screens

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,46 +8,48 @@ import Project from "./Project";
 const Projects = () => {
   return (
     <>
-      <Box
-        maxWidth="lg"
-        marginX="7%"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Typography variant="h2" color="primary">
-          <Box sx={{ textAlign: "center", m: 1 }}>Projects:</Box>
-        </Typography>
-        <Grid
-          container
-          spacing={1}
-          direction="row"
+      <Container>
+        <Box
+          maxWidth="lg"
+          marginX="7%"
+          display="flex"
+          flexDirection="column"
           justifyContent="center"
           alignItems="center"
-          alignContent="center"
-          wrap="wrap"
-          height={480}
-          sx={{ backgroundColor: "yellow" }}
         >
-          <Project title="Project 1" />
-          <Project title="Project 2" />
-          <Grid item xs={4}>
-            <Paper>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <Typography variant="h6" color="primary">
-                  Project
-                </Typography>
-              </Box>
-            </Paper>
+          <Typography variant="h2" color="primary">
+            <Box sx={{ textAlign: "center", m: 1 }}>Projects:</Box>
+          </Typography>
+          <Grid
+            container
+            spacing={1}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            alignContent="center"
+            wrap="wrap"
+            height={480}
+            sx={{ backgroundColor: "yellow" }}
+          >
+            <Project title="Project 1" />
+            <Project title="Project 2" />
+            <Grid item xs={4}>
+              <Paper>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Typography variant="h6" color="primary">
+                    Project
+                  </Typography>
+                </Box>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Container>
     </>
   );
 };
